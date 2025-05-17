02e8968acbd2dc278c7ba047dff348c3f8b61277
Enzo <enzonbarata@outlook.com>
2025-05-17 17:51:20 -0300

    feat: sorting by name to break the tie

--- src/routes/dados/+page.server.js
+++ src/routes/dados/+page.server.js
@@ -2,7 +2,7 @@ import { supabase } from "$lib/supabaseClient";
 
 export async function load({ params }) {
 	const allClassroomMapData = (await supabase.from('classroomMap').select('*').gt('day', '2024-02-04').order('day')).data;
-	const studentsData = (await supabase.from('students').select('id,name,left,late,year').overlaps('year', ['2024'])).data;
+	const studentsData = (await supabase.from('students').select('id,name,left,late,year').overlaps('year', ['2024']).order('name')).data;
 
 	return {studentsData, allClassroomMapData};
 }
--- src/routes/dados/2023/+page.server.js
+++ src/routes/dados/2023/+page.server.js
@@ -2,7 +2,7 @@ import { supabase } from "$lib/supabaseClient";
 
 export async function load({ params }) {
 	const allClassroomMapData = (await supabase.from('classroomMap').select('*').lt('day', '2023-12-12').order('day')).data;
-	const studentsData = (await supabase.from('students').select('id,name,left,late,year').overlaps('year', ['2023'])).data;
+	const studentsData = (await supabase.from('students').select('id,name,left,late,year').overlaps('year', ['2023']).order('name')).data;
 
 	return {studentsData, allClassroomMapData};
 }
