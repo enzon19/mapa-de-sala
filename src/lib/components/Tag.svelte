f9f27fe467e6ff583466d21e76b4f4e1deebc2f7
Enzo <enzonbarata@outlook.com>
2025-05-17 17:46:29 -0300

    feat: href prop added to tag

--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -2,11 +2,14 @@
   import tags from '$lib/data/tags.js'
 
   export let tagType;
+  export let href = "/tags";
   const tag = tags[tagType];
   const IconComponent = tag.icon;
 </script>
 
-<a href="/tags" class={`flex flex-row items-center justify-center text-center m-4 p-4 rounded-2xl ${tag.color}`}>
-  <IconComponent class="inline-block mr-2 w-16 h-16 sm:w-7 sm:h-7" />
+<svelte:element this={href ? 'a' : 'div'} {href} class={`flex flex-row items-center justify-center text-center m-4 p-4 rounded-2xl w-full mx-auto ${tag.color}`}>
+  <div class="w-max mr-2">
+    <IconComponent class="outline-none w-7 h-7" />
+  </div>
   <span>{@html tag.text}</span>
-</a>
+</svelte:element>
