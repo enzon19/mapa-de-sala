8b9beb9beb19ca8401a82ae2cd000655de33b6f5
Enzo <enzonbarata@outlook.com>
2025-05-17 17:50:00 -0300

    style & fix: new date format

--- src/routes/dia/[day=day]-[month=month]-[year=year]/+page.server.js
+++ src/routes/dia/[day=day]-[month=month]-[year=year]/+page.server.js
@@ -2,18 +2,36 @@ import { supabase } from "$lib/supabaseClient";
 import { DateTime } from "luxon";
 
 export async function load({ params, url }) {
-  const highlight = url.searchParams.get('destacar');
+  const highlight = url.searchParams.get("destacar");
 
-	// os parâmetros (/07-06-2023) fornecem o dia e o mês escolhidos pelo usuário e informados na URL
-	const requestedDate = DateTime.local(Number(params.year), Number(params.month), Number(params.day), 0, 0);
+  // os parâmetros (/07-06-2023) fornecem o dia e o mês escolhidos pelo usuário e informados na URL
+  const requestedDate = DateTime.local(
+    Number(params.year),
+    Number(params.month),
+    Number(params.day),
+    0,
+    0
+  );
 
-	let classroomMapData = (await supabase.from("classroomMap").select('*').eq('day', requestedDate.toString()).limit(1)).data[0] || []; // day, layout, tags
-	const layout = classroomMapData.columns; // informações sobre as fileiras e colunas (mapa de sala em si)
-	
-	const studentsData = (await supabase.from('students').select('*')).data; // puxar as pessoas
-	if (!layout) return {params, classroomMapData, studentsData}; // Se não tiver o layout/mapa de sala do dia, é melhor só retornar o que se tem
+  let classroomMapData =
+    (
+      await supabase
+        .from("classroomMap")
+        .select("*")
+        .eq("day", requestedDate.toISODate())
+        .limit(1)
+    ).data[0] || []; // day, layout, tags
+  const layout = classroomMapData.columns; // informações sobre as fileiras e colunas (mapa de sala em si)
 
-	studentsData.push({"id": "clssrmmp_empty", "name": ""}, {"id": "clssrmmp_space", "name": null});
-	classroomMapData.layout = layout.map(column => column.map(id => studentsData.find(student => student.id === id))); // substituir cada ID da array com o layout pelo objeto com o nome e id das pessoas
-	return {params, classroomMapData, studentsData, highlight};
+  const studentsData = (await supabase.from("students").select("*")).data; // puxar as pessoas
+  if (!layout) return { params, classroomMapData, studentsData }; // Se não tiver o layout/mapa de sala do dia, é melhor só retornar o que se tem
+
+  studentsData.push(
+    { id: "clssrmmp_empty", name: "" },
+    { id: "clssrmmp_space", name: null }
+  );
+  classroomMapData.layout = layout.map((column) =>
+    column.map((id) => studentsData.find((student) => student.id === id))
+  ); // substituir cada ID da array com o layout pelo objeto com o nome e id das pessoas
+  return { params, classroomMapData, studentsData, highlight };
 }
